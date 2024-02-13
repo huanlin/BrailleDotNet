a1ccbc4ace65524a49ca6dca0842c093db43b5bd
Michael Tsai <huanlin.tsai@gmail.com>
2024-02-13 23:30:53 +0800

    Beta version of English and Chinese braille symbols.

---
+++ dotnet/BrailleSymbol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateYaml
+{
+    /// <summary>
+    /// A BrailleSymbol represents a basic unit of a braille text. It can be a character, a word, or even words.
+    /// This class is supposed to support multiple languages, but currently only English and Traditional Chinese are supported.
+    /// </summary>
+    public class BrailleSymbol
+    {
+        public string Text { get; set; }
+        public List<string> Dots { get; set; } = new List<string>();
+        //public string? ShortText { get; set; }
+        public string? Description { get; set; }         
+    }
+}
